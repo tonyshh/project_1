a6bc84088886dd589565321dd255b75e7081c345
Anton Shalin <shalin.anton8@gmail.com>
2022-10-26 17:05:30 +1000

    add even

---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+from random import randint
+from prompt import string
+from brain_games.cli import welcome_user
+
+ROUNDS = 3
+counter = 0
+
+def main():
+    welcome_user()
+
+
+if __name__ == '__main__':
+    main()
+
+ 
+
+
+
+print('Answer "yes" if the number is even, otherwise answer "no".')
+for _ in range(ROUNDS):
+  number = randint(1,100)
+  if number%2 == 0:
+    correct_answer = 'yes'
+  else:
+    correct_answer = 'no'
+  print(f'Question: {number}')
+  players_answer = input()
+  print(f'Your answer: {players_answer}')
+  if players_answer == correct_answer:
+    print('Correct')
+    counter+=1
+  else:
+    print(f'"{players_answer}" is wrong answer ;(. Correct answer was "{correct_answer}"')
+    break
+  if counter == 3:
+    print('Congratulations!')
+
