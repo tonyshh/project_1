4be7abd31d94220472fc993f9888f8b3738666ac
Anton Shalin <shalin.anton8@gmail.com>
2022-10-26 17:21:12 +1000

    fix brain_even

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,22 +1,15 @@
 #!/usr/bin/env python3
 from random import randint
 from prompt import string
-from brain_games.cli import welcome_user
+
 
 ROUNDS = 3
 counter = 0
 
-def main():
-    welcome_user()
 
-
-if __name__ == '__main__':
-    main()
-
- 
-
-
-
+print('Welcome to the Brain Games!')
+name = string('May I have your name? ')
+print('Hello,', name + '!')
 print('Answer "yes" if the number is even, otherwise answer "no".')
 for _ in range(ROUNDS):
   number = randint(1,100)
@@ -34,5 +27,5 @@ for _ in range(ROUNDS):
     print(f'"{players_answer}" is wrong answer ;(. Correct answer was "{correct_answer}"')
     break
   if counter == 3:
-    print('Congratulations!')
+    print(f'Congratulations! {name}')
 
